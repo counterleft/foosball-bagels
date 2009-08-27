8de3814d6d39a9f16e7e4e00e1e2fc49348ddb00
Brian Mathiyakom <brian@rarevisions.net>
2009-08-26 22:57:19 -0700

    fix bug 1159928 -- All occurences of plus minus value should be colored

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,12 +1,12 @@
 module PlayersHelper
-  def colored_plus_minus(player)
-    if player.plus_minus > 0
+  def self.colored_plus_minus(plus_minus)
+    if plus_minus > 0
       css_class = "positive"
-    elsif player.plus_minus < 0
+    elsif plus_minus < 0
       css_class = "negative"
     else
       css_class = "neutral"
     end
-    %(<span class="#{css_class}">#{player.plus_minus}</span>)
+    %(<span class="#{css_class}">#{plus_minus}</span>)
   end
 end
--- test/unit/helpers/players_helper_test.rb
+++ test/unit/helpers/players_helper_test.rb
@@ -1,24 +1,21 @@
 require 'test_helper'
 
 class PlayersHelperTest < ActionView::TestCase
-  test "should return positive class for positive plus minus" do
-    p = Player.new
-    p.plus_minus = 1
-    plus_minus_markup = colored_plus_minus p
-    assert_equal %(<span class="positive">#{p.plus_minus}</span>), plus_minus_markup
+  test "should return neutral class for zero plus minus" do
+    plus_minus = 0
+    plus_minus_markup = PlayersHelper.colored_plus_minus plus_minus
+    assert_equal %(<span class="neutral">#{plus_minus}</span>), plus_minus_markup
   end
 
   test "should return negative class for negative plus minus" do
-    p = Player.new
-    p.plus_minus = -1
-    plus_minus_markup = colored_plus_minus p
-    assert_equal %(<span class="negative">#{p.plus_minus}</span>), plus_minus_markup
+    plus_minus = -1
+    plus_minus_markup = PlayersHelper.colored_plus_minus plus_minus
+    assert_equal %(<span class="negative">#{plus_minus}</span>), plus_minus_markup
   end
 
-  test "should return neutral class for zero plus minus" do
-    p = Player.new
-    p.plus_minus = 0
-    plus_minus_markup = colored_plus_minus p
-    assert_equal %(<span class="neutral">#{p.plus_minus}</span>), plus_minus_markup
+  test "should return positive class for positive plus minus" do
+    plus_minus = 1
+    plus_minus_markup = PlayersHelper.colored_plus_minus plus_minus
+    assert_equal %(<span class="positive">#{plus_minus}</span>), plus_minus_markup
   end
 end
