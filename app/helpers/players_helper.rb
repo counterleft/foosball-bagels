57a7d8c2dd90b7997c734eec744e2a390e467703
Brian Mathiyakom <brian@rarevisions.net>
2010-01-21 20:37:04 -0800

    players_helper should help render bagel fact sentences
    Some logic that creates the view copy for bagel preventers and contributors move into the PlayersHelper

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,4 +1,23 @@
 module PlayersHelper
+  
+  def self.bagel_contributors(contributors, bagel_contributor_links)
+    contribute_verb = " is "
+    contribute_verb = " are " if bagel_contributor_links.size > 1
+
+    plus_minus = PlayersHelper.colored_plus_minus contributors.first.plus_minus
+
+    bagel_contributor_links.to_sentence + contribute_verb + "more likely to help a team receive a bagel with a plus-minus of " + plus_minus + "."
+  end
+
+  def self.bagel_preventers(preventers, bagel_preventer_links)
+    prevent_verb = " is "
+    prevent_verb = " are " if bagel_preventer_links.size > 1
+
+    plus_minus = PlayersHelper.colored_plus_minus preventers.first.plus_minus
+
+    bagel_preventer_links.to_sentence + prevent_verb + "more likely to help a team give a bagel to another team with a plus-minus of " + plus_minus + "."
+  end
+
   def self.colored_plus_minus(plus_minus)
     if plus_minus > 0
       css_class = "positive"
