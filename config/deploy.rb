489ad5190323fa928809611f8c8f9c8b6dd08ed1
Brian Mathiyakom <brian@rarevisions.net>
2010-03-06 02:05:07 -0800

    complete story 2187149
    Add capistrano deployment setup

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "Bagel Central"
+set :repository,  "git://github.com/bmthykm/foosball-bagels.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, 
+# `perforce`, `subversion` or `none`
+
+role :web, "rarevisions.net"  # Your HTTP server, Apache/etc
+role :app, "rarevisions.net"  # This may be the same as your `Web` server
+role :db,  "rarevisions.net", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+set :port, 220
+
+set :deploy_to, "/var/www/foosball"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
