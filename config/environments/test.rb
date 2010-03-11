e79f4dc088b34f9130cf96436fe5536b6dad6e91
Brian Mathiyakom <brian@rarevisions.net>
2010-03-10 23:07:26 -0800

    make sure to use test-unit 1.2.3 with rspec-1.9.x

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,6 +28,7 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
 config.gem "rspec", :lib => false, :version => "1.2.9"
+config.gem "test-unit", :lib => false, :version => "1.2.3"
 config.gem "rspec-rails", :lib => false, :version => "1.2.9"
 config.gem "machinist", :version => "1.0.6"
 config.gem "faker", :version => "0.3.1"
