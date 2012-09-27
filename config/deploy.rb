50f72fea2de07f90886a4e554d68e2dc31ffd635
Brian Mathiyakom <brian@rarevisions.net>
2012-09-27 09:59:09 -0700

    Update deploy script for ruby 1.9.3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "bundler/capistrano"
 
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, '1.9.3-p194'        # Or whatever env you want it to run in.
 set :rvm_type, :user
 
 set :application, "Bagel Central"
