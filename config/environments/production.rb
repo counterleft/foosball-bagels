6dd72c3239684bc5a9e174c203f1f97c2579b969
Brian Mathiyakom <brian@rarevisions.net>
2013-02-18 20:16:43 -0800

    Attempt to add extra css manifest to asset precompile list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Foosball::Application.configure do
   config.assets.digest = true
    
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( bagels.js )
+  config.assets.precompile += %w( bagels.js bagels.css )
   
   # See everything in the log (default is :info)
   # config.log_level = :debug
