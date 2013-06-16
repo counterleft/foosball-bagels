55911bbd8d8dedcba31b84ec0584f5c4d3eb0ac6
Brian Mathiyakom <brian@rarevisions.net>
2013-06-15 20:46:55 -0700

    Begin switch to flat ui design.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
-//= require jquery
-//= require jquery_ujs
+//= require jquery-1.8.3.min
+//= require jquery-ui-1.10.3.custom.min
+//= require jquery.ui.touch-punch.min
+//= require bootstrap.min
+//= require bootstrap-select
+//= require bootstrap-switch
+//= require flatui-checkbox
+//= require flatui-radio
+//= require jquery.tagsinput
+//= require jquery.placeholder
+//= require jquery.stacktable
+//= require app
--- config/application.rb
+++ config/application.rb
@@ -12,19 +12,19 @@ module Foosball
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password
-    
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-    
+
     config.autoload_paths += %W( #{Rails.root}/app/sweepers #{Rails.root}/app/services #{Rails.root}/app/lib )
     config.action_controller.page_cache_directory = Rails.root.to_s + "/public/cache"
 
     # Enable the asset pipeline
     config.assets.enabled = true
-     
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-     
+
     # Change the path that assets are served from
     # config.assets.prefix = "/assets"
   end
