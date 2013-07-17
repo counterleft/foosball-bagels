bba83d6c45b5857b353125513a1a2f7e4beba32a
Brian Mathiyakom <brian@rarevisions.net>
2013-07-17 00:16:11 -0700

    Remove page sweepers for now.
    Need to investigate replacement.

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,7 @@ module Foosball
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    config.autoload_paths += %W( #{Rails.root}/app/sweepers #{Rails.root}/app/services #{Rails.root}/app/lib )
-    config.action_controller.page_cache_directory = Rails.root.to_s + "/public/cache"
+    config.autoload_paths += %W(#{Rails.root}/app/services #{Rails.root}/app/lib)
 
     # Enable the asset pipeline
     config.assets.enabled = true
