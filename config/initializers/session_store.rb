5dc6c06095d1ec86bd6d62e4c5effa97eff4d8df
Brian Mathiyakom <brian@rarevisions.net>
2013-11-01 03:56:38 -0700

    US3 Move secret key base to external location in prod.
    Also remove rails 3 secret token since we'll purely use the rails 4
    variation.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
-Foosball::Application.config.session_store :cookie_store, :key => '_f00$b@ll_session'
-Foosball::Application.config.secret_token = "#wed$7Rb$9DikeoeiO9hgBneughCmAnekUeiS"
-Foosball::Application.config.secret_key_base = ")#(R2309th2309fj09gh0293t0$u32gslkgh"
+SECRET_KEY_BASE_FILE = File.join(Rails.root.to_s, '..', '..', '..', '..', 'shared', 'config', 'webapps', 'foosball', 'secret_key_base')
+
+secret_key_base= "testingkey9812741289473%%%%%"
+if File.readable?(SECRET_KEY_BASE_FILE)
+  secret_key_base = File.read(SECRET_KEY_BASE_FILE).chomp
+end
+
+Foosball::Application.config.session_store :cookie_store, :key => '_bcs'
+Foosball::Application.config.secret_key_base = secret_key_base
