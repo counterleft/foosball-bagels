f0c72b592ff09d262369b868fda6305e0ea857d2
Brian Mathiyakom <brian@rarevisions.net>
2013-11-01 17:37:05 -0700

    Clean up production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,6 @@ Foosball::Application.configure do
 
   config.eager_load = true
 
-  # Choose the compressors to use
-  # config.assets.js_compressor  = :uglifier
-  # config.assets.css_compressor = :yui
-
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
@@ -26,22 +22,4 @@ Foosball::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( bagels.js highcharts.js picker.js picker.date.js picker-classic.css picker-classic.date.css jquery.pjax.js )
-
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
-
-  # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable threaded mode
-  # config.threadsafe!
 end
